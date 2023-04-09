125286a6561465ba26d5436548768e9f546bc948
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-04-09 16:15:20 +0400

    Add files via upload

---
+++ OrderBy 1 and 2.sql
@@ -0,0 +1,21 @@
+SELECT
+	employee_id,
+	first_name,
+	last_name,
+	salary
+FROM
+	employees
+	ORDER BY 
+	last_name
+
+
+
+SELECT
+	department_id,
+	department_name,
+	location_id
+
+FROM
+	departments
+	ORDER BY
+	location_id
