dd2a8cfddd5dce3602467c47285dcb9f6cbb8fa8
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-04-18 00:35:14 +0400

    Add files via upload

---
+++ inner join task.sql
@@ -0,0 +1,21 @@
+﻿/* Seattle şəhərində yerləşən şöbələrdə hansı işçilər işləyir? */
+
+SELECT * FROM employees
+
+SELECT * FROM departments
+
+SELECT * FROM locations
+
+/* department_id & location_id */
+
+SELECT
+	first_name,
+	last_name,
+	email,
+	l.city /* kodu yazanda mene komek olsun */
+FROM 
+	employees e
+INNER JOIN departments d ON d.department_id = e.department_id
+INNER JOIN locations l ON l.location_id = d.location_id
+WHERE
+	l.location_id IN (1700)
