7e47b4ebe492bf315c2a00d52a7d6b390919845b
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-04-27 21:21:27 +0400

    Add files via upload

---
+++ left join task.sql
@@ -0,0 +1,25 @@
+﻿/* 1.Hansı ölkələrin əlaqəli şöbələri yoxdur? */
+
+SELECT * FROM countries
+SELECT * FROM locations
+SELECT * FROM departments
+
+
+SELECT
+	c.country_id,
+	l.country_id,
+	d.department_id,
+	d.location_id
+FROM countries c
+LEFT JOIN locations l ON l.location_id = c.country_id
+LEFT JOIN departments d ON d.location_id = l.location_id
+WHERE
+	c.country_id <> l.country_id
+
+
+
+/* 2.Bütün işçilərin adları və e-poçt ünvanları və onların menecerlərinin adları və e-poçt ünvanlarını (əgər varsa) bir araya gətirin. */
+SELECT
+	first_name, last_name, email, manager_id
+FROM
+	employees
