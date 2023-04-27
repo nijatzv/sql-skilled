5eddc2c99c33cb60119d02d6eb565c6b103d54cb
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-04-28 00:26:23 +0400

    Add files via upload

---
+++ sum function task.sql
@@ -0,0 +1,22 @@
+﻿/* Hər bir departament üçün ümumi əmək haqqı nə qədərdir? */
+
+SELECT * FROM departments
+SELECT * FROM employees
+
+
+SELECT
+	e.department_id,
+	d.department_name,
+	SUM(salary)
+FROM
+	employees e
+INNER JOIN departments d ON d.department_id = e.department_id
+GROUP BY
+	e.department_id,
+	department_name
+ORDER BY
+	SUM(salary) DESC;
+
+
+
+
