bf95eb7161124599efb85f728b2f69aa6eb729d1
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-04-28 12:09:18 +0400

    Add files via upload

---
+++ avg function task.sql
@@ -0,0 +1,17 @@
+﻿/* 'Marketing Manager' vəzifəsi olan bütün işçilər üçün orta əmək haqqı nə qədərdir? */
+
+SELECT * FROM employees
+SELECT * FROM jobs
+
+SELECT
+	e.job_id,
+	AVG(salary),
+	job_title
+FROM
+	employees e
+INNER JOIN jobs j ON j.job_id = e.job_id
+WHERE
+	e.job_id = 10
+GROUP BY
+	e.job_id,
+	job_title
