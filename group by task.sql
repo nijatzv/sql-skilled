8d75476e5c88337c648d3c1f9fa6b1e1e14d6475
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-04-30 00:28:09 +0400

    Add files via upload

---
+++ group by task.sql
@@ -0,0 +1,14 @@
+﻿							/* Hər bir job title üçün orta əmək haqqını göstərən SQL sorğusu yazın. */
+
+SELECT * FROM jobs
+SELECT * FROM employees
+
+
+SELECT
+	j.job_title,
+	AVG(salary) avg_salary
+FROM
+	employees e
+INNER JOIN jobs j ON j.job_id = e.job_id
+GROUP BY
+	j.job_title
