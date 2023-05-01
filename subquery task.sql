262cc3c6bc7e7aca4b28f30b6ed6b4d5eee10f05
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-05-01 23:52:23 +0400

    Add files via upload

---
+++ subquery task.sql
@@ -0,0 +1,17 @@
+﻿					/* Alt sorğu ilə işçilərinin əmək haqqı 5.000 dollardan çox olan şöbələri tapın. */
+
+SELECT * FROM departments
+SELECT * FROM employees
+
+SELECT 
+	department_name
+FROM 
+	departments
+WHERE 
+	department_id IN (
+	SELECT 
+		department_id
+	FROM 
+		employees
+	WHERE salary > 5000
+);
