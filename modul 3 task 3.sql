997909a065761590a99ce3c7d14b316fa0d753c2
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-06-01 18:00:08 +0400

    Add files via upload

---
+++ modul 3 task 3.sql
@@ -0,0 +1,16 @@
+﻿-- TASK 3 (DELETE)
+-- dependents cədvəlindən employee_id 101,102 və 103 olan sətirləri silin. Bunu bir sorğu yazaraq edin.
+
+DELETE FROM dependents
+WHERE 
+	employee_id IN (101, 102, 103)
+
+SELECT
+	COUNT(*)
+FROM
+	dependents
+WHERE
+	employee_id IN (101, 102, 103);
+
+SELECT employee_id
+FROM dependents
