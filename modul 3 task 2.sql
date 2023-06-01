9e45bc14946f71d2deb05b2d0d923cb7275d12fa
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-06-01 17:36:24 +0400

    Add files via upload

---
+++ modul 3 task 2.sql
@@ -0,0 +1,12 @@
+﻿-- TASK 2 (UPDATE)
+-- "Employees" cədvəlində bir sətir üzrə iki sütunu yeniləyin.
+
+UPDATE employees
+SET 
+	first_name = 'Nicat',
+	last_name = 'Zekerye'  -- updated from 'Nijat Zakarya' to 'Nicat Zekerye'
+WHERE 
+	employee_id = 208
+
+select * from employees
+where employee_id = 208
