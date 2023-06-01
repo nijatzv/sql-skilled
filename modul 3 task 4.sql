505e61feea12ec7162f5b7511a66f5128021518e
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-06-01 18:15:59 +0400

    Add files via upload

---
+++ modul 3 task 4.sql
@@ -0,0 +1,11 @@
+﻿-- TASK 4 (CREATE)
+-- 4 sütunlu yeni bir cədvəl yaradın.
+
+CREATE TABLE books(
+	book_id INT PRIMARY KEY,
+	book_name VARCHAR(50),
+	type VARCHAR(50),
+	pages INT
+);
+
+SELECT * FROM books
