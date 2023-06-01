be8d8ec958dab532b6ec9e77f62cefdd72ec74f0
Nijat Zakarya <90623233+nijatzv@users.noreply.github.com>
2023-06-01 17:29:23 +0400

    Add files via upload

---
+++ modul 3 task 1.sql
@@ -0,0 +1,32 @@
+﻿-- TASK 1 (INSERT)
+
+--"Employees" cədvəlinə bütün sütunlar üzrə uyğun qiymətlər daxil edin.
+
+
+INSERT INTO employees (
+	first_name, last_name, email, phone_number,
+	hire_date, job_id, salary, manager_id, department_id
+)
+VALUES 
+	(
+		'Nijat', 'Zakarya', 'zekeryenicat@gmail.com', '012.345.6789', 
+		'2023-04-01', 9, 6000.00, 103, 6
+
+	);
+
+	SELECT * FROM employees
+
+
+	(select
+		manager_id
+	from
+		employees
+	where
+		employees.job_id = 9);
+
+		(select
+		department_id
+	from
+		employees
+	where
+		manager_id = 103);
